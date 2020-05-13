567ab556db99fa9506e82836be4389c62fec818c
Hayato Nakamura <33381712+hayatonakamura@users.noreply.github.com>
2020-05-12 22:35:00 -0400

    Update and rename new_hit_with_camera.py to board/new_hit_with_camera.py

--- board/new_hit_with_camera.py
+++ board/new_hit_with_camera.py
@@ -20,7 +20,7 @@ def search(mat):
 			window = mat[y:y+3, x:x+3]
 			print(window)
 			print(np.mean(window))
-			if (np.mean(window) > 34 and np.mean(window) < 45):
+			if (np.mean(window) > 36 and np.mean(window) < 45):
 				print("\n\nHIT\n\n")
 				return True
 	return False
